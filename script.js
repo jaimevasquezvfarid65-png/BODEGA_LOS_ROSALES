4f237e015c493ac7386059f46504a8513c3ef505
Jaime Farid Vásquez Valdez <jaimevasquezvfarid65@gmail.com>
2026-04-21 13:04:08 -0500

    Finalice el registro

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const btn = document.getElementById("btn");
-const contenedor = document.querySelector(".contenedor")
-btn.addEventListener("click",()=>{
-    contenedor.classList.toggle("toggle");
+const contenedor = document.querySelector(".contenedor");
+const btnSignIn = document.getElementById("btn-sign-in");
+const btnSignUp = document.getElementById("btn-sign-up");
+
+btnSignIn.addEventListener("click",()=>{
+    contenedor.classList.remove("toggle");
+});
+btnSignUp.addEventListener("click",()=>{
+    contenedor.classList.add("toggle");
+    
 });
